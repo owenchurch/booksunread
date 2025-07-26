05ff610b884a3ed9474a9f04995e968f8eb4578a
owenchurch <owenchurch@gmail.com>
2025-07-26 18:58:46 -0400

    Initial Commit

---
+++ booksunread/booksunreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksunreadApp.swift
+//  booksunread
+//
+//  Created by Owen Church on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct booksunreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ booksunreadTests/booksunreadTests.swift
@@ -0,0 +1,17 @@
+//
+//  booksunreadTests.swift
+//  booksunreadTests
+//
+//  Created by Owen Church on 7/26/25.
+//
+
+import Testing
+@testable import booksunread
+
+struct booksunreadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
